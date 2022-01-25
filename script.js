75ba842ad48e9b518a23cef6b8dda0e2e0a05d44
DanielVec <vechioladaniel@gmail.com>
2022-01-24 20:58:37 -0800

    edit strings to add more information

--- script.js
+++ script.js
@@ -22,17 +22,17 @@ function oneRound(playerSelection, computerSelection) {
 
 
 
-    console.log(playerSelection, computerSelection)
+    console.log("You chose " + playerSelection + ". The computer chose " + computerSelection + ".")
     //if user input is rock and computer is scissors say you win
     if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore += 1
-        console.log("You win.")
+        console.log("You win. Rock beats Paper")
     }
         
     //if user input is rock and computer is paper say you lose
     else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore +=  1
-        console.log("You Lose.")
+        console.log("You Lose. Paper beats Rock.")
     }
     // tie
     else if (playerSelection === "rock" && computerSelection === "rock") {
@@ -41,22 +41,22 @@ function oneRound(playerSelection, computerSelection) {
     }
     //repeat for other scenarios
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You win.")
+        console.log("You win. Paper beats Rock.")
         playerScore += 1
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You Lose.")
+        console.log("You Lose. Scissors beats Paper")
         computerScore += 1
     }
     else if (playerSelection === "paper" && computerSelection === "paper") {
         console.log("Tie.")
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You win.")
+        console.log("You win. Scissors beats Paper.")
         playerScore += 1
     }
     else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You Lose.")
+        console.log("You Lose. Rock beats Scissors")
         computerScore += 1
     }
     else if (playerSelection === "scissors" && computerSelection === "scissors") {
@@ -69,18 +69,24 @@ function oneRound(playerSelection, computerSelection) {
 function game() {
 
     oneRound()
+    console.log("Score: User " + playerScore + " Computer " + computerScore)
     oneRound()
+    console.log("Score: User " + playerScore + " Computer " + computerScore)
     oneRound()
+    console.log("Score: User " + playerScore + " Computer " + computerScore)
     oneRound()
+    console.log("Score: User " + playerScore + " Computer " + computerScore)
     oneRound()
+    console.log("Score: User " + playerScore + " Computer " + computerScore)
+
     if (computerScore > playerScore) {
-        console.log("You lose.")
+        console.log("Game over. You lose.")
     }
     else if (computerScore < playerScore) {
-        console.log("You win.")
+        console.log("Game over. You win.")
     }
     else {
-        console.log("Tie.")
+        console.log("Game over. Tie.")
     }
 }
 game()
